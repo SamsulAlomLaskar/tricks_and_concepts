1344caf64439cfeb506ad1e6da8ac265667bc4fc
Samsul Alom Laskar <laskarikbal126@gmail.com>
2023-04-09 18:41:47 +0530

    condition added for every, i.e & operator

--- condition.js
+++ condition.js
@@ -2,4 +2,5 @@ let y = 10;
 
 if (y == 5 || y == 8 || y == 10 || y == 4) console.log("Hey");
 
-if ([5, 8, 10, 4].includes(y)) console.log("Hey Short");
+if ([5, 8, 10, 4].includes(y)) console.log("Hey Includes");
+if ([10, 10, 10].every((val) => val == y)) console.log("Hey Every");
