208cdfc2449f6a569f0fdee61ebd7e98a0ad3884
Samsul Alom Laskar <91590153+SamsulAlomLaskar@users.noreply.github.com>
2023-05-19 23:45:37 +0530

    added a comment regarding the octal number

--- condition.js
+++ condition.js
@@ -4,3 +4,5 @@ if (y == 5 || y == 8 || y == 10 || y == 4) console.log("Hey");
 
 if ([5, 8, 10, 4].includes(y)) console.log("Hey Includes");
 if ([10, 10, 10].every((val) => val == y)) console.log("Hey Every");
+
+//? In JavaScript, numbers that start with zero followed by other digits are interpreted as octal (base 8) numbers. The octal number 050 is represented in base 10 as 40.
